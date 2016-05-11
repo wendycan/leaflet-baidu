928cd4c4b4edb15951daf8b52f252676c82e2ff5
Wendy <wendycaner@gmail.com>
2016-05-11 20:47:02 +0800

    Create translate_point.js

---
+++ layer/tile/translate_point.js
@@ -0,0 +1,17 @@
+window._translatePoint = function (originalPoint) {
+  var i = 0;
+  return new Promise(function(resolve, reject){
+    var baiduPoint;
+    var flag;
+    var convertor = new BMap.Convertor();
+    convertor.translate([originalPoint], 1, 5, function(data){
+        baiduPoint = data.status === 0 ? data.points[0] : null;
+        resolve(baiduPoint);
+    });
+  });
+};
+
+async function TranslatePoint(originalPoint){
+  var data = await _translatePoint(originalPoint);
+  return data;
+};
